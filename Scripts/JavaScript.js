f31fdec1c0ad892c085004b1a43376ea24e85cab
FontaineKillian <kfontaine5@myges.fr>
2025-06-03 18:07:12 +0200

    yep

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const audio = document.getElementById("bg-music");
-    audio.volume = 0.1; // Volume bas
+    audio.volume = 0.05; // Volume bas
     // Tente de jouer le son si permis
     const playPromise = audio.play();
     if (playPromise !== undefined) {
